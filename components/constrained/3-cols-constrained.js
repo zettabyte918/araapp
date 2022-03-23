35dbba48263316bb210ee314198f42799b228ea2
Houssem Hammami <hammamihossem99@gmail.com>
2022-03-23 16:36:38 +0100

    edit

--- components/constrained/3-cols-constrained.js
+++ components/constrained/3-cols-constrained.js
@@ -110,21 +110,21 @@ function ThreeColsConstrained() {
                 </div>
               </div>
 
-              <Disclosure.Panel className="lg:hidden">
+              <Disclosure.Panel className="lg:hidden text-right">
                 <div className="px-2 pt-2 pb-3">
                   <Disclosure.Button
                     as="a"
                     href="#"
                     className="block px-3 py-2 rounded-md text-base font-medium text-white bg-indigo-800"
                   >
-                    Dashboard
+                    الصفحة الرئيسية
                   </Disclosure.Button>
                   <Disclosure.Button
                     as="a"
                     href="#"
                     className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
                   >
-                    Support
+                    المجموعات
                   </Disclosure.Button>
                 </div>
                 <div className="pt-4 pb-3 border-t border-indigo-800">
@@ -134,21 +134,21 @@ function ThreeColsConstrained() {
                       href="#"
                       className="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
                     >
-                      Your Profile
+                      المقالات
                     </Disclosure.Button>
                     <Disclosure.Button
                       as="a"
                       href="#"
                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
                     >
-                      Settings
+                      الصور
                     </Disclosure.Button>
                     <Disclosure.Button
                       as="a"
                       href="#"
                       className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600"
                     >
-                      Sign out
+                      الوثائق
                     </Disclosure.Button>
                   </div>
                 </div>
